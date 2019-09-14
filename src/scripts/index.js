30670ae098999900e2bb924bc67d28e8d8321692
Safdar Jamal <safdar.dev@gmail.com>
2019-09-14 23:13:12 +0500

    CRUD functioning

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,6 @@ import { View } from './view.js';
 import { Controller } from './controller.js';
 
 const app = new Controller(new Model(), new View());
+
+app.model.addTodo('Learn HTMl');
+console.log(app.model.todos);
