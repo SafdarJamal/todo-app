8b433ff7c2bbda7064ab5caf86e12b2b861bff3b
Safdar Jamal <safdar.dev@gmail.com>
2019-09-14 22:59:51 +0500

    Create addTodo method

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -6,6 +6,16 @@ class Model {
       { id: 2, text: 'Plant a garden', complete: false }
     ];
   }
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false
+    };
+
+    this.todos.push(todo);
+  }
 }
 
 export { Model };
