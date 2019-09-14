327315c59c04af12efde35234bff4d2ad041f342
Safdar Jamal <safdar.dev@gmail.com>
2019-09-14 23:28:21 +0500

    Model dealing with data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,5 +4,9 @@ import { Controller } from './controller.js';
 
 const app = new Controller(new Model(), new View());
 
-app.model.addTodo('Learn HTMl');
+app.model.addTodo('Learn JavaScript');
+app.model.editTodo(3, 'Learn JavaScript and React');
+app.model.deleteTodo(2);
+app.model.toggleTodo(1);
+
 console.log(app.model.todos);
