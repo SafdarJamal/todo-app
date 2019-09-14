984ae9fa8f759e8aa5802f64a96cc25535a8707d
Safdar Jamal <safdar.dev@gmail.com>
2019-09-14 22:54:29 +0500

    Initialize state

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -1,5 +1,11 @@
 class Model {
-  constructor() {}
+  constructor() {
+    // The state of the model, an array of todo objects, prepopulated with some data
+    this.todos = [
+      { id: 1, text: 'Run a marathon', complete: false },
+      { id: 2, text: 'Plant a garden', complete: false }
+    ];
+  }
 }
 
 export { Model };
