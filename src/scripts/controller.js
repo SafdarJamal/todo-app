24f7752ac1015e45fd4dff0b0dcc8419ace258c5
Safdar Jamal <safdar.dev@gmail.com>
2019-09-15 17:40:45 +0500

    create handlers for the events in the controller

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -2,7 +2,30 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+
+    // Display initial todos
+    this.onTodoListChanged(this.model.todos);
   }
+
+  onTodoListChanged = todos => {
+    this.view.displayTodos(todos);
+  };
+
+  handleAddTodo = todoText => {
+    this.model.addTodo(todoText);
+  };
+
+  handleEditTodo = (id, todoText) => {
+    this.model.editTodo(id, todoText);
+  };
+
+  handleDeleteTodo = id => {
+    this.model.deleteTodo(id);
+  };
+
+  handleToggleTodo = id => {
+    this.model.toggleTodo(id);
+  };
 }
 
 export { Controller };
