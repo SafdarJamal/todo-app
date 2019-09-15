c36d39d594782e02e93edd414c4fae6bf4fdc66f
Safdar Jamal <safdar.dev@gmail.com>
2019-09-15 17:09:04 +0500

    Make helper methods in View, to retrieve an element and create an element.

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,5 +1,20 @@
 class View {
   constructor() {}
+
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+
+    return element;
+  }
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector);
+
+    return element;
+  }
 }
 
 export { View };
